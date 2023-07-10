51ad25253cf99085d33a755f7e5337b36648c59d
Marlon Morejón <lake0888@gmail.com>
2023-07-10 20:02:05 +0000

    Completed Challenge: Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,24 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
     }
+
+    /**
+     * 
+     * public static int fib(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] fibs = new int[n + 1];
+        fibs[0] = 0;
+        fibs[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        }
+        return fibs[n];
+    }
+     */
 }
